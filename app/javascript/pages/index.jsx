a1e9fbae29493edc782e7265cf3214e07b4dae87
BelakJG <74761299+BelakJG@users.noreply.github.com>
2026-05-12 15:34:28 -0400

    Added styling and changed icon in browser tab

--- app/javascript/pages/index.jsx
+++ app/javascript/pages/index.jsx
@@ -1,6 +1,7 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "../components/App";
+import "../styles/index.css";
 
 export default document.addEventListener("DOMContentLoaded", () => {
     const container = document.getElementById("root");
