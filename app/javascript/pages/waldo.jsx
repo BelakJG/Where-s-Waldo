91ea295f72249c0465712dcf542d1c20b4704d5e
BelakJG <74761299+BelakJG@users.noreply.github.com>
2026-05-14 13:00:52 -0400

    calculates click position to add box on screen

--- app/javascript/pages/waldo.jsx
+++ app/javascript/pages/waldo.jsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import waldo from "../images/waldo.jpeg"
 import "../styles/waldo"
 
 export default function Waldo() {
-    return(<div id="waldo">
+    const [ boxes, setBoxes ] = useState([]);
+    function placeDiv(event) {
+        const waldo = document.getElementById("waldo");
+        const rect = waldo.getBoundingClientRect();
+        const x = event.clientX - rect.left + waldo.scrollLeft;
+        const y = event.clientY - rect.top + waldo.scrollTop;
+        console.log(`x: ${x}, y: ${y}`);
+
+        setBoxes([...boxes, {x: x, y: y}]);
+    }
+
+    const rightBoxes = boxes.map((box, index) => <div style={{
+        width: "50px",
+        height: "100px",
+        border: "4px dashed #39FF14",
+        outline: "2px solid white",
+        position: "absolute",
+        left: `${box.x}px`,
+        top: `${box.y}px`,
+        transform: "translate(-50%, -50%)"
+    }} key={index}></div>);
+
+    return(<div id="waldo" onClick={placeDiv}>
+        {rightBoxes}
         <img src={waldo} alt="find waldo pic"></img>
     </div>);
 }
