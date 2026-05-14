f46275cbdbb3c22f2ced3f5f3393fd0297a60a76
BelakJG <74761299+BelakJG@users.noreply.github.com>
2026-05-14 14:26:50 -0400

    Added handler for determining if a guess is right possible guesses are in a state that removes them after a correct guess to prevent flooding of boxes implemented scrolling horizontally so it can be played on all screen sizes

--- app/javascript/pages/waldo.jsx
+++ app/javascript/pages/waldo.jsx
@@ -1,17 +1,34 @@
 import React, { useState } from "react";
-import waldo from "../images/waldo.jpeg"
-import "../styles/waldo"
+import waldo from "../images/waldoBackground.jpeg"
+import "../styles/waldo.css"
 
 export default function Waldo() {
+    
     const [ boxes, setBoxes ] = useState([]);
-    function placeDiv(event) {
-        const waldo = document.getElementById("waldo");
-        const rect = waldo.getBoundingClientRect();
-        const x = event.clientX - rect.left + waldo.scrollLeft;
-        const y = event.clientY - rect.top + waldo.scrollTop;
-        console.log(`x: ${x}, y: ${y}`);
+    const [ characters, setCharacters] = useState([
+        {name: "waldo", x: 1980, y: 645, image: waldo}
+    ]);
+    
+    function makeGuess(event, name) {
+        const waldoContainer = document.getElementById("waldo");
+        const guess = characters.find(character => character.name === name);
+        if (!guess) {
+            console.log("waldo.jsx: character not found");
+            return;
+        }
+        const rect = waldoContainer.getBoundingClientRect();
+        const x = event.clientX - rect.left + waldoContainer.scrollLeft;
+        const y = event.clientY - rect.top + waldoContainer.scrollTop;
+        console.log(`click x: ${x}, y: ${y}`);
 
-        setBoxes([...boxes, {x: x, y: y}]);
+        if (Math.abs(x - guess.x) <= 30 && Math.abs(y - guess.y) <= 30) {
+            setBoxes(boxes => [...boxes, {x: guess.x, y: guess.y}]);
+            console.log(`Correct! You found ${name}!`);
+            setCharacters(characters => characters.filter(character => character.name !== name));
+        } else {
+            console.log(`Guess for ${name} incorrect, try again`);
+        }
+
     }
 
     const rightBoxes = boxes.map((box, index) => <div style={{
@@ -25,7 +42,7 @@ export default function Waldo() {
         transform: "translate(-50%, -50%)"
     }} key={index}></div>);
 
-    return(<div id="waldo" onClick={placeDiv}>
+    return(<div id="waldo" onClick={(event) => makeGuess(event, "waldo")}>
         {rightBoxes}
         <img src={waldo} alt="find waldo pic"></img>
     </div>);
