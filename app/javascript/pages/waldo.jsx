88d95efa8735928e4d4b233b99e77346c12460ad
BelakJG <74761299+BelakJG@users.noreply.github.com>
2026-05-14 16:44:25 -0400

    Clicking on picture now opens a menu to make guess clicking elsewhere while menu is visible closes it

--- app/javascript/pages/waldo.jsx
+++ app/javascript/pages/waldo.jsx
@@ -11,44 +11,70 @@ export default function Waldo() {
         {name: "Waldo", x: 1585, y: 600, image: characterWaldo},
         {name: "Odlaw", x: 275, y: 565, image: characterOdlaw}
     ]);
+    const [menuValues, setMenuValues ] = useState({
+        visible: false,
+        x: 0,
+        y: 0
+    });
+
+    function openMenu(event) {
+        const waldoContainer = document.getElementById("waldo");
+        const rect = waldoContainer.getBoundingClientRect();
+        const x = event.clientX - rect.left + waldoContainer.scrollLeft;
+        const y = event.clientY - rect.top + waldoContainer.scrollTop;
+        setMenuValues({
+            visible: true,
+            x: x,
+            y: y
+        });
+    }
+
+    function closeMenu() {
+        setMenuValues({visible: false, x: 0, y: 0});
+    }
     
-    function makeGuess(event, name) {
+    function makeGuess(x, y, name) {
         const waldoContainer = document.getElementById("waldo");
         const guess = characters.find(character => character.name === name);
         if (!guess) {
             console.log("waldo.jsx: character not found");
             return;
         }
-        const rect = waldoContainer.getBoundingClientRect();
-        const x = event.clientX - rect.left + waldoContainer.scrollLeft;
-        const y = event.clientY - rect.top + waldoContainer.scrollTop;
         console.log(`click x: ${x}, y: ${y}`);
 
-        if (Math.abs(x - guess.x) <= 30 && Math.abs(y - guess.y) <= 30) {
+        if (Math.abs(x - guess.x) <= 30 && Math.abs(y - guess.y) <= 50) {
             setBoxes(boxes => [...boxes, {x: guess.x, y: guess.y}]);
             console.log(`Correct! You found ${name}!`);
             setCharacters(characters => characters.filter(character => character.name !== name));
         } else {
             console.log(`Guess for ${name} incorrect, try again`);
         }
+        closeMenu();
     }
 
     const rightBoxes = boxes.map((box, index) => <div style={{
         left: `${box.x}px`,
         top: `${box.y}px`
-    }} key={index} className="correctGuess"></div>);
+    }} key={`box-${index}`} className="correctGuess"></div>);
 
     const possibleCharacters = characters.map((character, index) => <div className="character" key={`charcter-${index}`}>
         <img src={character.image}></img>
         <p>{character.name}</p>
     </div>);
 
-    return(<div id="waldo" onClick={(event) => makeGuess(event, "Waldo")}>
+    const characterGuesses = characters.map((character, index) => <button type="button" 
+                                                                    key={`button-${index}`} 
+                                                                    onClick={() => makeGuess(menuValues.x, menuValues.y, character.name)}>
+                                                                    <h2>{character.name}</h2>
+                                                                </button>);
+
+    return(<div id="waldo">
         {rightBoxes}
         <div id="possibleGuesses" style={{backgroundColor: characters.length > 0 ? "rgba(255, 255, 255, .75)" : "white"}}>
-            { characters.length > 0 ? <h2>Characters to find</h2> : <h2>All characters found!</h2> }
+            { characters.length > 0 ? <h2>Characters left to find</h2> : <h2>All characters found!</h2> }
             <div className="characters">{possibleCharacters}</div>
         </div>
-        <img src={waldoBackground} alt="find waldo pic"></img>
+        <div id="guessMenu" style={{left: menuValues.x, top: menuValues.y, display: menuValues.visible ? "flex" : "none"}}>{characterGuesses}</div>
+        <img src={waldoBackground} alt="find waldo pic" onClick={!menuValues.visible ? openMenu : closeMenu}></img>
     </div>);
 }
