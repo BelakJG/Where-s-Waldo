b15e933519172639efa40542c9e69f5944522c75
BelakJG <74761299+BelakJG@users.noreply.github.com>
2026-05-14 19:08:05 -0400

    added last character to waldo

--- app/javascript/pages/waldo.jsx
+++ app/javascript/pages/waldo.jsx
@@ -2,13 +2,16 @@ import React, { useState } from "react";
 import waldoBackground from "../images/waldoBackground.jpeg";
 import characterWaldo from "../images/characterWaldo.webp";
 import characterOdlaw from "../images/characterOdlaw.webp";
+import characterWoof from "../images/characterWoof.webp";
+
 import GuessingGame from "../components/guessingGame";
 import "../styles/guessingGame.css";
 
 export default function Waldo() {
     const characters = [
         {name: "Waldo", x: 1585, y: 600, image: characterWaldo},
-        {name: "Odlaw", x: 275, y: 565, image: characterOdlaw}
+        {name: "Odlaw", x: 275, y: 565, image: characterOdlaw},
+        {name: "Woof", x: 1743, y: 573, image: characterWoof}
     ];
 
     return(<GuessingGame characterData={characters} backgroundImage={waldoBackground} pageName={"waldo"} />);
